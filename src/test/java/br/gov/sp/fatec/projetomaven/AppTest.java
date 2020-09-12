2b2ef0f6c681a8be9b8072cc55d7a76e78070dd2
otavio-raposo <yuusak.otavio@gmail.com>
2020-09-12 15:35:57 -0300

    first commit

---
+++ src/test/java/br/gov/sp/fatec/projetomaven/AppTest.java
@@ -0,0 +1,23 @@
+package br.gov.sp.fatec.projetomaven;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	/**
+	 * Rigorous Test :-)
+	 */
+	@Test
+    public void testSoma()
+    {
+		Calculadora calc = new Calculadora();
+        assertTrue( calc.soma() == 2 );
+    }
+}
